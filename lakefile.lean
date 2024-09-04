8ddd205cc3fd0d758a54dd8da41be183d3142070
Lean 4 VS Code Extension <>
2024-09-04 09:27:51 +0800

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "example2" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Example2» where
+  -- add any library configuration options here
