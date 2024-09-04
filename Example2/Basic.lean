f7e93dd14af22f7ff391c462c180f79f16d53656
matt <mattchen05@gmail.com>
2024-09-04 09:56:20 +0800

    blueprint2

--- Example2/Basic.lean
+++ Example2/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+import Mathlib.Tactic
+
+def one := 1
+
+def two := 2
+
+theorem one_plus_one_eq_two : one + one = two := by
+  rfl
+
+def one_plus_one_eq_two_with_def : Prop := one + one = two
